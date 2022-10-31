bff022980de6ae1288d064bbf719d1a56c921db9
kurata321 <kurata.hironori321@gmail.com>
2022-10-31 23:03:17 +0900

    add serverSideProps

--- pages/border/index.tsx
+++ pages/border/index.tsx
@@ -1,9 +1,40 @@
-const Index = () => {
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const Index: NextPage<Props> = (props) => {
+  const todo: Todo[] = props.todo;
   return (
     <div>
-      border
+      <h1>border</h1>
+      <ul>
+        {todo.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Index;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const todo = await response.json();
+  return {
+    props: {
+      todo,
+    },
+  };
+};
